840757681a3d20164251d5233f1a8fe0ea8897fa
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-06-19 19:22:00 +0530

    feat: banner added

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,8 @@
+import HomePage from "@widgets/Home";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Hello World!!!</>
+    <HomePage/>
   );
 }
---
+++ frontend/src/widgets/Home/Banner/view.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '@styles/banner.scss'
+
+export default function Banner() {
+  return (
+    <div className='bg-black w-full container justify-center'>
+      <span className="bannerTitle">NSS HSS KAVALAM</span>
+    </div>
+  )
+}
