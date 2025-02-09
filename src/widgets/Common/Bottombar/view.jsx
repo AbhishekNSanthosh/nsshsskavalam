fb8928dbfcafd357932bb04fd1a244ff820ff3da
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-02-09 20:23:20 +0530

    feat: getting ready for production

--- src/widgets/Common/Bottombar/view.jsx
+++ src/widgets/Common/Bottombar/view.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Bottombar() {
   return (
-    <div className='w-full px-[5vw] py-4 flex items-center justify-center'>
-        <div className="w-full flex gap-2 flex-col md:flex-row lg:flex-row items-center lg:justify-between border-t border-gray-300 pt-10 pb-5">
-            <div className="flex md:flex-1 lg:flex-1">
-            <span className="text-font-secondary text-xs md:text-base lg:text-base">© 2024 NSS HSS, Kavalam. All Rights Reserved.</span>
-            </div>
-            <div className="flex md:flex-1 lg:flex-1 items-center justify-end">
-                <span className="text-font-secondary text-xs md:text-base lg:text-base">Crafted by Midnitcode Innovations</span>
-            </div>
+    <div className="w-full px-[5vw] py-4 flex items-center justify-center">
+      <div className="w-full flex gap-2 flex-col md:flex-row lg:flex-row items-center lg:justify-between border-t border-gray-300 pt-10 pb-5">
+        <div className="flex md:flex-1 lg:flex-1">
+          <span className="text-font-secondary text-xs md:text-base">
+            © <span className="font-sans">2025</span> NSS HSS, Kavalam. All
+            Rights Reserved.
+          </span>
         </div>
+        <div className="flex md:flex-1 lg:flex-1 items-center justify-end">
+          <span className="text-font-secondary text-xs md:text-base lg:text-base">
+            Crafted by{" "}
+            <Link target="_blank" className="font-semibold" href={"https://midnitcode.in"}>
+              Midnitcode Innovations
+            </Link>
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
