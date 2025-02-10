3c36842d0afce0a3a860c182ccac319f18174aa4
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-02-10 06:49:54 +0530

    feat: font changed

--- src/widgets/Academics/Components/Content.jsx
+++ src/widgets/Academics/Components/Content.jsx
@@ -4,10 +4,10 @@ import React from "react";
 
 export default function Content() {
   return (
-    <div className="px-5vw py-8">
+    <div className="px-5vw py-8 pt-[90px] lg:pt-0">
       <div className="flex flex-col gap-8">
         <TitleBar title={"Academics"} />
-        <div className="w-full flex flex-col gap-4">
+        <div className="w-full flex flex-col gap-4" data-aos="fade-up">
           {academics.map((item,index) => (
             <div className="flex flex-col gap-2" key={index}>
               <span className="text-xl capitalize font-medium text-primary-400">
--- src/widgets/GeneralRules/view.jsx
+++ src/widgets/GeneralRules/view.jsx
@@ -4,16 +4,16 @@ import { DisciplinaryRules } from '../../common/Utils/utils'
 
 export default function GeneralRules() {
   return (
-    <div className='w-screen py-8 px-5vw'>
+    <div className='w-screen py-8 px-5vw pt-[80px] lg:pt-0'>
       <div className="flex flex-col gap-8">
         <TitleBar title={"Rules and Regulations"}/>
-        <div className="">
+        <div className="" data-aos="fade-up">
           <p className="text-lg text-justify">We expect our students, their parents / guardians and the family members concerned to co-operate with us to maintain discipline in the school. We believe that no disciplinary action needs to be taken against any child,but indiscipline should be nipped in the bud. Therefore, pay attention to the following disciplinary rules.</p>
         </div>
-        <div className="">
+        <div className="" data-aos="fade-up">
           <span className="text-2xl font-semibold text-primary">DISCIPLINARY RULES</span>
         </div>
-        <div className="w-full flex flex-col gap-1">
+        <div className="w-full flex flex-col gap-1" data-aos="fade-up">
           {DisciplinaryRules?.map((rule,index)=>(
             <div className="w-full" key={`${rule}_${index}`}>
               <p className="text-lg">{index+1}.{" "}{rule}</p>
