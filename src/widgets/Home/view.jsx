b228b1acdfdfcc949099093c0a73ab2f4401a101
Abhishek N Santhosh <97347843+AbhishekNSanthosh@users.noreply.github.com>
2025-12-23 07:56:19 +0530

    Update view.jsx

--- src/widgets/Home/view.jsx
+++ src/widgets/Home/view.jsx
@@ -1,19 +1,21 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import Banner from '@widgets/Home/Banner'
-import Message from '@widgets/Home/Message'
-import About from '@widgets/Common/About'
-import Counter from '@widgets/Home/Counter'
-import Wish from '@widgets/Home/Wish'
-import Founder from '@widgets/Home/Founder'
-import OurTeam from './OurTeam'
-import PreLoader from '@components/PreLoader'
-import Video from './Videos'
-import GalleryView from './Gallery'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Banner from "@widgets/Home/Banner";
+import Message from "@widgets/Home/Message";
+import About from "@widgets/Common/About";
+import Counter from "@widgets/Home/Counter";
+import Wish from "@widgets/Home/Wish";
+import Founder from "@widgets/Home/Founder";
+import OurTeam from "./OurTeam";
+import PreLoader from "@components/PreLoader";
+import Video from "./Videos";
+import GalleryView from "./Gallery";
+import AdBanner from "@/app/AdBanner";
 
 export default function HomePage() {
   const [isLoaded, setIsLoaded] = useState(false);
-  
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoaded(true);
@@ -21,18 +23,32 @@ export default function HomePage() {
 
     return () => clearTimeout(timer);
   }, []);
+
   return (
     <div>
-        {!isLoaded && <PreLoader />}
-        <Banner/>
-        <Founder/>
-        <About/>
-        <OurTeam/>
-        <Message/>
-        <Counter/>
-        <Video/>
-        <GalleryView/>
-        <Wish/>
+      {!isLoaded && <PreLoader />}
+
+      <Banner />
+
+      {/* 🔥 Ad after banner */}
+      <AdBanner />
+
+      <Founder />
+      <About />
+
+      {/* 🔥 Ad after About */}
+      <AdBanner />
+
+      <OurTeam />
+      <Message />
+      <Counter />
+
+      {/* 🔥 Ad before videos */}
+      <AdBanner />
+
+      <Video />
+      <GalleryView />
+      <Wish />
     </div>
-  )
+  );
 }
